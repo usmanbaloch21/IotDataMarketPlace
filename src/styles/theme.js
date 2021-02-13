d9f02308b31675e5ef9f8e86cca874ed181fd2a5
usmanbaloch21 <63309886+usmanbaloch21@users.noreply.github.com>
2021-02-13 06:25:34 +0000

    Add files via upload

---
+++ src/styles/theme.js
@@ -0,0 +1,8 @@
+import { blue,green } from '@material-ui/core/colors';
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export default createMuiTheme({
+  palette: {
+    green:green
+  },
+});
